92f63a9510db2ca7124684677675bfcc99329e35
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-05-02 13:16:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+try :
+    # past here your link 
+    page = requests.get('https://forecast.weather.gov/MapClick.php?lat=34.05349000000007&lon=-118.24531999999999')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    week= soup.find(id="seven-day-forecast-body")
+
+    items = week.find_all(class_="tombstone-container")
+    #print(items[0])
+
+    print(items[0].find(class_="period-name").get_text())
+    print(items[0].find(class_="short-desc").get_text())
+    print(items[0].find(class_="temp").get_text())
+
+    period_names = [item.find(class_="period-name").get_text() for item in items]
+    #print(period_names)
+
+    short_descriptions = [item.find(class_="short-desc").get_text() for item in items]
+    #print(short_descriptions)
+
+    temperature = [item.find(class_="temp").get_text() for item in items]
+    #print(temperature)
+
+    wether_stuff = pd.DataFrame(
+        {'period': period_names,
+         'short_descriptions': short_descriptions,
+          'temperature': temperature,
+        }
+    )
+    print(wether_stuff)
+    wether_stuff.to_csv('first.csv')
+except :
+
+    print("https://forecast.weather.gov \n \n      go there select your location and past url.")
